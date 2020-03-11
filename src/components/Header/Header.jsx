86bf239a5dd8f0c8e086ff28fd79e2eda1c73b44
FreeDOSSS <dima15521@gmail.com>
2020-03-11 21:54:11 +0200

    start header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as style from './Header.module.css';
+
+function Header() {
+  return (
+    <div className={style.fluid}>
+      <div className={style.container}></div>
+    </div>
+  );
+}
+
+export default Header;
