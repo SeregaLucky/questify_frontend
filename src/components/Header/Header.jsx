649699efe4dc6753d1edce24d8b37459cd6b6628
FreeDOSSS <dima15521@gmail.com>
2020-03-12 22:39:59 +0200

    header mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import * as style from './Header.module.css';
+import { Link } from 'react-router-dom';
 
-function Header() {
+function Header({ userName = 'Jorem Ipsum' }) {
   return (
     <div className={style.fluid}>
-      <div className={style.container}></div>
+      <div className={style.container}>
+        {/* Logo */}
+        <Link className={style.logo_link}>Questify</Link>
+
+        {/* Name */}
+        <div className={style.user}>
+          <div className={style.firstLetter}>{userName[0]}</div>
+          <p className={style.name}>{userName}</p>
+        </div>
+
+        {/* Future */}
+        <div className={style.icon}></div>
+      </div>
     </div>
   );
 }
