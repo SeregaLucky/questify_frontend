ca47ea356c2e84cda52032625d87ca1bb424661e
Your Name <kateryna.skliaruk6@gmail.com>
2020-03-15 10:48:01 +0100

    some changes in authForm - tablet resolution

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './AuthPage.module.css';
+import AuthForm from '../../components/AuthForm';
 
 const AuthPage = () => {
   return (
     <>
       <h1 className={styles.title}>AuthPage</h1>
+      <AuthForm />
     </>
   );
 };
