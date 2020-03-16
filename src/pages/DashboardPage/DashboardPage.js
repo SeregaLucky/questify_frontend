26c7b4c50384fb47b4deeecf53c43498f53cdceb
“anstarky” <anstarky@gmail.com>
2020-03-16 20:33:12 +0200

    init anstarky

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
 import Header from '../../components/Header/Header';
+import CreateQuestButton from '../../components/CreateQuestButton';
 
 const DashboardPage = () => {
   return (
     <>
       <Header />
+
+      <CreateQuestButton />
     </>
   );
 };
