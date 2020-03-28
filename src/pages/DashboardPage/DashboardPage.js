a9e3d6686bc3270b864f0b6075b4f932711d747b
SergiyMi <bladeneo@i.ua>
2020-03-28 11:19:25 +0200

    delete shortid and add destructuring

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -30,6 +30,8 @@ class DashboardPage extends Component {
   };
 
   render() {
+    const { collection } = this.props;
+    const { isDoneOpen, isOpen } = this.state;
     return (
       <>
         <Header />
@@ -37,13 +39,10 @@ class DashboardPage extends Component {
           <div className={styles.today}>
             <p className={styles.text}>today</p>
 
-            {/* <TodayList quests={this.props.collection.today} /> */}
-            {this.state.isOpen ? (
-              <CardContainer questData={{}} newCard={true} />
-            ) : null}
+            {isOpen ? <CardContainer questData={{}} newCard={true} /> : null}
 
-            {this.props.collection.today.map(item => (
-              <div key={shortid.generate()} className={styles.card}>
+            {collection.today.map(item => (
+              <div key={item._id} className={styles.card}>
                 <CardContainer
                   questData={{
                     questId: item._id,
@@ -62,8 +61,8 @@ class DashboardPage extends Component {
 
           <div className={styles.tomorrow}>
             <p className={styles.text}>tomorrow</p>
-            {this.props.collection.tomorrow.map(item => (
-              <div key={shortid.generate()} className={styles.card}>
+            {collection.tomorrow.map(item => (
+              <div key={item._id} className={styles.card}>
                 <CardContainer
                   questData={{
                     questId: item._id,
@@ -82,8 +81,8 @@ class DashboardPage extends Component {
 
           <div className={styles.other}>
             <p className={styles.text}>other</p>
-            {this.props.collection.other.map(item => (
-              <div key={shortid.generate()} className={styles.card}>
+            {collection.other.map(item => (
+              <div key={item._id} className={styles.card}>
                 <CardContainer
                   questData={{
                     questId: item._id,
@@ -101,7 +100,7 @@ class DashboardPage extends Component {
           </div>
 
           <div className={styles.done}>
-            {this.state.isDoneOpen ? (
+            {isDoneOpen ? (
               <p className={styles.text} onClick={this.handleClick}>
                 done &#9650;
               </p>
@@ -110,9 +109,9 @@ class DashboardPage extends Component {
                 done &#9660;
               </p>
             )}
-            {this.state.isDoneOpen
-              ? this.props.collection.done.map(item => (
-                  <div key={shortid.generate()} className={styles.card}>
+            {isDoneOpen
+              ? collection.done.map(item => (
+                  <div key={item._id} className={styles.card}>
                     <CardContainer
                       questData={{
                         questId: item._id,
@@ -130,7 +129,7 @@ class DashboardPage extends Component {
               : null}
             <CreateQuestButton
               handleClick={this.handleClickCreate}
-              isOpen={this.state.isOpen}
+              isOpen={isOpen}
             />
           </div>
         </div>
